31bb00d41c03dfa0e026c57120cb062bd06e990b
YamitGC <yamitgc08@gmail.com>
2026-03-04 20:30:12 -0500

    ATM V1.1 yamit

---
+++ CAJERO/app/logica_atm.py
@@ -0,0 +1,30 @@
+def consultar_saldo(usuario):
+    saldo = cuentas.get(usuario, 0)
+    print(f"\n--- CONSULTA ---")
+    print(f"Usuario: {usuario.upper()}")
+    print(f"Tu saldo actual es: ${saldo:.2f}")
+
+def depositar_dinero(usuario):
+    try:
+        monto = float(input("¿Cuánto deseas depositar?: "))
+        if monto > 0:
+            cuentas[usuario] += monto
+            print(f"¡Depósito exitoso! Nuevo saldo: ${cuentas[usuario]:.2f}")
+        else:
+            print("Error: El monto debe ser mayor a 0.")
+    except ValueError:
+        print("Error: Entrada inválida. Por favor, ingresa solo números.")
+
+def retirar_dinero(usuario):
+    try:
+        monto = float(input("¿Cuánto deseas retirar?: "))
+        if 0 < monto <= cuentas[usuario]:
+            cuentas[usuario] -= monto
+            print(f"Retiro exitoso. Saldo restante: ${cuentas[usuario]:.2f}")
+        elif monto > cuentas[usuario]:
+            print("Error: Fondos insuficientes.")
+        else:
+            print("Error: El monto debe ser mayor a 0.")
+    except ValueError:
+        print("Error: Entrada inválida. Por favor, ingresa solo números.")
+        
---
+++ CAJERO/app/progress_bar.py
@@ -0,0 +1,13 @@
+import time
+import sys
+
+def progress_bar(total=10):
+    print("Cargando...")
+    for i in range(total + 1):
+        percent = int((i/ total) * 100)
+        bar = "█" * i + "-" * (total - i)
+        sys.stdout.write(f"\r[{bar}] {percent}%")
+        sys.stdout.flush()
+        time.sleep(0.1)
+    print("\nCompletado!\n")
+
